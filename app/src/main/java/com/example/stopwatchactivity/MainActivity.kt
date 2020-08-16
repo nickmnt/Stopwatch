96fff31a9863e95266d7115c05824eff873c2dc8
Nima Moein <nimamt@users.noreply.github.com>
2020-08-16 19:16:33 +0430

    Create basic methods for click and the main logic of the timer

--- app/src/main/java/com/example/stopwatchactivity/MainActivity.kt
+++ app/src/main/java/com/example/stopwatchactivity/MainActivity.kt
@@ -3,22 +3,42 @@ package com.example.stopwatchactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var running = false
+    var seconds = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun startClicked(view: View) {
-        
+        running = true
     }
 
     fun stopClicked(view: View) {
-
+        running = false
     }
 
     fun restartClicked(view: View) {
+        running = false
+        seconds = 0
+    }
 
+    fun updateTimer() {
+        //Clearly this stopwatch does NOT account for drift
+        val hours = seconds/3600
+        val minutes = (seconds%3600)/60
+        val seconds1 = seconds%60
+
+        val time = String.format(Locale.getDefault(),
+        "%d:%02d:%02d", hours, minutes, seconds1)
+        textView.text = time
+        if(running)
+            seconds++
     }
 }
