15d110472a07113a448f5e8f8d1e8ff7d6d0b5b3
Nima Moein <nimamt@users.noreply.github.com>
2020-08-16 16:37:14 +0430

    Initial commit

---
+++ app/src/main/java/com/example/stopwatchactivity/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.stopwatchactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun startClicked(view: View) {
+        
+    }
+
+    fun stopClicked(view: View) {
+
+    }
+
+    fun restartClicked(view: View) {
+
+    }
+}
