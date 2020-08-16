5fd51b358618696affd0c5ae313ed99bc508241c
Nima Moein <nimamt@users.noreply.github.com>
2020-08-16 19:22:22 +0430

    Changed function name, added handler

--- app/src/main/java/com/example/stopwatchactivity/MainActivity.kt
+++ app/src/main/java/com/example/stopwatchactivity/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.stopwatchactivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -14,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createTimerAndRun()
     }
 
     fun startClicked(view: View) {
@@ -29,16 +32,22 @@ class MainActivity : AppCompatActivity() {
         seconds = 0
     }
 
-    fun updateTimer() {
-        //Clearly this stopwatch does NOT account for drift
-        val hours = seconds/3600
-        val minutes = (seconds%3600)/60
-        val seconds1 = seconds%60
+    fun createTimerAndRun() {
+        val handler = Handler()
+        handler.post(object : Runnable {
+            override fun run() {
+                //Clearly this stopwatch does NOT account for drift
+                val hours = seconds/3600
+                val minutes = (seconds%3600)/60
+                val seconds1 = seconds%60
 
-        val time = String.format(Locale.getDefault(),
-        "%d:%02d:%02d", hours, minutes, seconds1)
-        textView.text = time
-        if(running)
-            seconds++
+                val time = String.format(Locale.getDefault(),
+                    "%d:%02d:%02d", hours, minutes, seconds1)
+                textView.text = time
+                if(running)
+                    seconds++
+                handler.postDelayed(this, 1000)
+            }
+        })
     }
 }
