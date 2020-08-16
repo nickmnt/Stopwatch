d39b3f43ac2336ffca97d3f570a3a93a22072eac
Nima Moein <nimamt@users.noreply.github.com>
2020-08-16 19:24:54 +0430

    Made function private, replaced deprecated code

--- app/src/main/java/com/example/stopwatchactivity/MainActivity.kt
+++ app/src/main/java/com/example/stopwatchactivity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.stopwatchactivity
 
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -32,22 +33,20 @@ class MainActivity : AppCompatActivity() {
         seconds = 0
     }
 
-    fun createTimerAndRun() {
-        val handler = Handler()
-        handler.post(object : Runnable {
-            override fun run() {
-                //Clearly this stopwatch does NOT account for drift
-                val hours = seconds/3600
-                val minutes = (seconds%3600)/60
-                val seconds1 = seconds%60
+    private fun createTimerAndRun() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            //Clearly this stopwatch does NOT account for drift
+            val hours = seconds / 3600
+            val minutes = (seconds % 3600) / 60
+            val seconds1 = seconds % 60
 
-                val time = String.format(Locale.getDefault(),
-                    "%d:%02d:%02d", hours, minutes, seconds1)
-                textView.text = time
-                if(running)
-                    seconds++
-                handler.postDelayed(this, 1000)
-            }
-        })
+            val time = String.format(
+                Locale.getDefault(),
+                "%d:%02d:%02d", hours, minutes, seconds1
+            )
+            textView.text = time
+            if (running)
+                seconds++
+        }, 3000)
     }
 }
